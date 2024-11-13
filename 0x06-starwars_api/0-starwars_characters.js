d0c98fefe5a127766953d5b9658e3afb096ae11f
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-11-13 14:16:46 +0100

    task done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
+/**
+ * This script fetches and prints the names of all characters from a specified
+ * Star Wars movie, based on the movie ID provided as a command-line argument.
+ * It uses the Star Wars API (https://swapi-api.hbtn.io/) to retrieve the movie 
+ * details, then makes additional requests to get each character's name in the 
+ * same order as listed in the API.
+ * 
+ * Usage: node script.js <filmID>
+ * Example: node script.js 3
+ */
 const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
