ca8e7d1414463eaa7e5f2b06f927362b31e7c7e2
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-11-30 10:39:15 +0100

    task done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-#a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+""" method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""
 
 def minOperations(n):
     if n < 2:
-        return 0  # Impossible to achieve
+        return 0
 
     operations = 0
     divisor = 2
