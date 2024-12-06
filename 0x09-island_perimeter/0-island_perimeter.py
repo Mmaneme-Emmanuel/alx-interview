7015059ab082138c574e029086a52b784594a10d
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-12-06 23:50:33 +0100

    task done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-""" function def island_perimeter(grid): that returns
-the perimeter of the island described in grid:
-"""
+"""Defines island perimeter finding function."""
 
 
 def island_perimeter(grid):
-    perimeter = 0
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    # Dimensions of the grid
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:  # Land cell
-                # Start with 4 edges
-                perimeter += 4
-
-                # Check top neighbor
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Shared edge
-
-                # Check left neighbor
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Shared edge
-
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
